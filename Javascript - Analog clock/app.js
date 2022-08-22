70118d23f0cddbd09b7ac1ba9d3f0129fd7da608
Regan Jansen <61661064+regandrinkstoomuchbeer@users.noreply.github.com>
2022-08-22 12:11:38 +0200

    Add files via upload
    A basic Analog clock created with Javascript that provides real-time time

---
+++ Javascript - Analog clock/app.js
@@ -0,0 +1,17 @@
+const secondHand = document.getElementById('seconds-hand')
+const minutesHand =  document.getElementById(' minutes-hand')
+const hoursHand = document.getElementById('hours-hand') 
+
+function getTime() {
+    const now  = new Date()
+    const seconds = now.getSeconds()
+    const minutes = now.getMinutes()
+    const hours = now.getHours()
+    const timeinterval = 6
+
+    secondHand.style.transform = ' rotate(' +(seconds * timeinterval) + 'deg )'
+    minutesHand.style.transform = ' rotate(' +(minutes * timeinterval + seconds/ 10) + 'deg )'
+    hoursHand.style.transform = ' rotate(' +(hours * 30 + minutes/ 2) + 'deg )'
+}
+    
+    setInterval(getTime , 100)
